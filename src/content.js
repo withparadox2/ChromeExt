89f3c677aabefde67685925b611ee07f42815c6d
withparadox2 <withparadox2@gmail.com>
2020-04-30 15:40:09 +1200

    Save and restore scroll position for archive.org

--- src/content.js
+++ src/content.js
@@ -8,7 +8,10 @@ if (testSite('nytimes.com')) {
     copyToClipboard(ele.src)
     window.close()
   }
+} else if (testSite('spotify.com')) {
+  removeElement('[data-testid="test-ref-div"]')
 } else if (testSite('archive.org')) {
+  setupArchive()
 }
 
 function testSite(input) {
@@ -45,4 +48,55 @@ function copyToClipboard(str) {
   el.select();
   document.execCommand('copy');
   document.body.removeChild(el);
+}
+
+function setupArchive() {
+  if (document.location.href.indexOf('.txt') < 0) {
+    return
+  }
+
+  const preEle = document.querySelector('.container pre')
+  var progressEle = null;
+
+  document.addEventListener('scroll', () => {
+    updateView(calPercent())
+    savePosition()
+  })
+
+  restorePosition()
+  updateView(calPercent())
+
+  function calPercent() {
+    const preEleShowBottom = document.scrollingElement.clientHeight - preEle.getBoundingClientRect().top
+    var percent = preEleShowBottom / preEle.clientHeight
+    if (percent > 1) {
+      percent = 1
+    }
+    return (percent * 100).toFixed(2)
+  }
+
+  function updateView(percent) {
+    if (progressEle == null) {
+      progressEle = document.createElement("div")
+      document.querySelector('#maincontent').appendChild(progressEle)
+
+      progressEle.style.position = 'fixed'
+      progressEle.style.right = (preEle.getBoundingClientRect().left + 10) + 'px'
+      progressEle.style.bottom = '10px'
+      progressEle.style.fontSize = '20px'
+
+      window.addEventListener('resize', () => {
+        progressEle.style.right = (preEle.getBoundingClientRect().left + 10) + 'px'
+      })
+    }
+    progressEle.innerText = percent
+  }
+
+  function savePosition() {
+    window.localStorage.setItem(document.location.href, document.scrollingElement.scrollTop)
+  }
+
+  function restorePosition() {
+    document.scrollingElement.scrollTop = window.localStorage.getItem(document.location.href) || 0
+  }
 }
