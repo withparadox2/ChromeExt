7bd35067a857de246253f4c66cf3c1c2886ae379
withparadox2 <withparadox2@gmail.com>
2020-05-09 23:04:18 +1200

    Shortcuts

--- src/content.js
+++ src/content.js
@@ -276,4 +276,87 @@ function setupYouglish() {
   if (videoContainer) {
     videoContainer.style.width = '65%'
   }
+
+  window.onkeydown = (function (ev) {
+    let key
+    let isShift
+    if (window.event) {
+      key = window.event.keyCode
+      isShift = !!window.event.shiftKey
+    } else {
+      key = ev.which
+      isShift = !!ev.shiftKey
+    }
+    console.log(key + ' ' + isShift)
+    if (isShift) {
+      switch (key) {
+        case 16: // ignore shift key
+          break
+        case 188: // <
+          adjustSpeed(true)
+          break
+        case 190: // >
+          adjustSpeed(false)
+          break
+        case 37: // <-
+          lastOrNextSentence(true)
+          break
+        case 39: // ->
+          lastOrNextSentence(false)
+          break
+        case 32: // space
+          loop()
+          break
+        default:
+          break
+      }
+    } else {
+      switch (key) {
+        case 37: // <-
+          backStep()
+          break;
+        case 32: // space
+          pausePlay()
+          break;
+      }
+    }
+  });
+
+  function adjustSpeed(isSlowDown) {
+    if (isSlowDown) {
+      document.querySelector('.togglespeed.slower').click()
+    } else {
+      document.querySelector('.togglespeed.faster').click()
+    }
+  }
+
+  function backStep() {
+    document.querySelector('#b_back').click()
+  }
+
+  function pausePlay() {
+    document.querySelector('#b_pause').click()
+  }
+
+  function loop() {
+    document.querySelector('.hand.ac_loopmode').click()
+  }
+
+  function lastOrNextSentence(isLast) {
+    const list = captionList.children[0].children
+    let curInd = -1
+    for (let i = 0; i < list.length; i++) {
+      if (list[i].className.indexOf('ac_current_cap') >= 0) {
+        curInd = i
+        break
+      }
+    }
+
+    if (curInd < 0 || curInd == 0 && isLast || curInd == list.length - 1 && !isLast) {
+      return
+    }
+
+    const nextInd = curInd + (isLast ? -1 : 1)
+    list[nextInd].querySelector('.hand').click()
+  }
 }
