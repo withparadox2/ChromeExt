ce29d2f55296ee21c0d466a930c0be4870211143
withparadox2 <withparadox2@gmail.com>
2020-05-09 17:11:05 +1200

    Parse youtube subtitle links

--- src/content.js
+++ src/content.js
@@ -17,6 +17,8 @@ if (testSite('nytimes.com')) {
   setupArchive()
 } else if (testSite('wekan.tv')) {
   setupWekanTv()
+} else if (testSite('youtube.com/watch')) {
+  window.onload = setupYoutube
 }
 
 function testSite(input) {
@@ -206,4 +208,26 @@ function setupWekanTv(consumedTime) {
       showEle.style.visibility = 'hidden'
     }, 3000)
   }
+}
+
+function setupYoutube() {
+  const scripts = [...document.querySelectorAll('script')].filter(node => node.innerText.indexOf('playerCaptionsRenderer":{"baseUrl') > 0)
+  if (scripts.length <= 0) {
+    console.log('Can not find script with text: "playerCaptionsRenderer..."')
+    return
+  }
+
+  const matchResult = scripts[0].innerText.match(/"playerCaptionsRenderer":{"baseUrl":"(.*?)",/)
+  if (matchResult.length < 2) {
+    console.log('Can not parse base url')
+    return
+  }
+  const baseUrl = matchResult[1].replace(/\\u0026/g, '&')
+  //tlangs: if enable translation
+  const list = baseUrl + '&type=list&asrs=1&tlangs=0'
+  const asr = baseUrl + '&type=track&lang=en&kind=asr'
+  console.log('baseurl:' + baseUrl)
+  console.log('listurl:' + list)
+  console.log('asr url:' + asr)
+
 }
