293203c3f2bc963ff07b7805b65e77135a2e298e
withparadox2 <withparadox2@gmail.com>
2020-05-23 13:47:47 +1200

    Highlight feedly title of readed item

--- src/feedly.js
+++ src/feedly.js
@@ -3,11 +3,12 @@ export function setup() {
   let cacheLength = 0
   setInterval(() => {
     const nodeList = [...document.querySelectorAll('.entry')]
-    if (!nodeList) {
-      cacheLength == 0
-    } else if (nodeList.length != cacheLength) {
+    if (nodeList.length != cacheLength) {
       cacheLength = nodeList.length
       nodeList.forEach(item => {
+        if (item.className.indexOf('unread') < 0) {
+          item.querySelector('.title').style.color = 'blue'
+        }
         item.onclick = (e) => {
           window.open(item.attributes['data-alternate-link'].value, '_blank')
           setTimeout(() => {
